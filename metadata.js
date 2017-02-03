3dbcf38096c609942c4f1921030618d88071f7c8
Andrey Bukati <andrey@bukati.me>
2017-02-03 15:12:09 -0800

    Update metadata.js

--- metadata.js
+++ metadata.js
@@ -20,8 +20,8 @@ module.exports.do = function(req, res){
             "args":[
                 {name:"consumerKey", type:"credentials", info:"The consumer key obtained from Tweeter.", required: true},
                 {name:"consumerSecret", type:"credentials", info:"The consumer secret obtained from Tweeter.", required: true},
-                {name:"accessTokenKey", type:"String", info:"The access token key obtained from Tweeter.", required: true},
-                {name:"accessTokenSecret", type:"String", info:"The access token secret obtained from Tweeter.", required: true},
+                {name:"accessTokenKey", type:"String", info:"The access token key obtained from Tweeter.", required: true, generator: "http://rapidoauth.com/twitter"},
+                {name:"accessTokenSecret", type:"String", info:"The access token secret obtained from Tweeter.", required: true, generator: "http://rapidoauth.com/twitter"},
                 {name:"tweetBody", type:"String", info:"The status you would like to tweet.", required: true}
             ],
             "callbacks":[
@@ -35,8 +35,8 @@ module.exports.do = function(req, res){
             "args":[
                 {name:"consumerKey", type:"credentials", info:"The consumer key obtained from Tweeter.", required: true},
                 {name:"consumerSecret", type:"credentials", info:"The consumer secret obtained from Tweeter.", required: true},
-                {name:"accessTokenKey", type:"String", info:"The access token key obtained from Tweeter.", required: true},
-                {name:"accessTokenSecret", type:"String", info:"The access token secret obtained from Tweeter.", required: true},
+                {name:"accessTokenKey", type:"String", info:"The access token key obtained from Tweeter.", required: true, generator: "http://rapidoauth.com/twitter"},
+                {name:"accessTokenSecret", type:"String", info:"The access token secret obtained from Tweeter.", required: true, generator: "http://rapidoauth.com/twitter"},
                 {name:"query", type:"String", info:"The search will be using this query.", required: true}
 
             ],
@@ -51,8 +51,8 @@ module.exports.do = function(req, res){
             "args":[
                 {name:"consumerKey", type:"credentials", info:"The consumer key obtained from Tweeter.", required: true},
                 {name:"consumerSecret", type:"credentials", info:"The consumer secret obtained from Tweeter.", required: true},
-                {name:"accessTokenKey", type:"String", info:"The access token key obtained from Tweeter.", required: true},
-                {name:"accessTokenSecret", type:"String", info:"The access token secret obtained from Tweeter.", required: true},
+                {name:"accessTokenKey", type:"String", info:"The access token key obtained from Tweeter.", required: true, generator: "http://rapidoauth.com/twitter"},
+                {name:"accessTokenSecret", type:"String", info:"The access token secret obtained from Tweeter.", required: true, generator: "http://rapidoauth.com/twitter"},
                 {name:"tweetId", type:"String", info:"The ID of the tweet to retweet.", required: true}
             ],
             "callbacks":[
@@ -66,8 +66,8 @@ module.exports.do = function(req, res){
             "args":[
                 {name:"consumerKey", type:"credentials", info:"The consumer key obtained from Tweeter.", required: true},
                 {name:"consumerSecret", type:"credentials", info:"The consumer secret obtained from Tweeter.", required: true},
-                {name:"accessTokenKey", type:"String", info:"The access token key obtained from Tweeter.", required: true},
-                {name:"accessTokenSecret", type:"String", info:"The access token secret obtained from Tweeter.", required: true},
+                {name:"accessTokenKey", type:"String", info:"The access token key obtained from Tweeter.", required: true, generator: "http://rapidoauth.com/twitter"},
+                {name:"accessTokenSecret", type:"String", info:"The access token secret obtained from Tweeter.", required: true, generator: "http://rapidoauth.com/twitter"},
                 {name:"tweetId", type:"String", info:"The ID of the tweet to retweet.", required: true},
                 {name:"tweetAddition", type:"String", info:"The addition to the tweet.", required: true}
             ],
